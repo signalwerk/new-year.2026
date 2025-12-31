024239245bfb8aafcbbb477c62ba4441d97917ea
signalwerk <sh@signalwerk.ch>
2025-12-31 15:35:30 +0100

    FIX: final text

--- src/config/text-luan.ts
+++ src/config/text-luan.ts
@@ -5,14 +5,14 @@
 import { TextConfig } from "./text";
 
 export const TEXT_LUAN: TextConfig = {
-  title: "LUAN\nPlatformer",
+  title: "LUAN\nJump",
   subtitle: "Alles Gute im neuen Jahr!",
-  copy: "Hast du am 23.3.2025 schon Pläne?\nIch würde gerne einen kleinen\nAusflug mit dir machen.",
+  copy: "Ich würde mich freuen, \nwenn wir uns am 10. Mai 2026 \nfür einen Ausflug treffen.\n\nHier schon mal ein kleines Spiel \nfür die Zeitvertreibung.",
   startButton: "Start!",
 
   win: {
     title: "Geschafft!",
-    copy: "Herzlichen Glückwunsch, Luan!\nDu hast das Portal erreicht!\n\nVergiss nicht:\n23. März 2025 – unser Ausflug!",
+    copy: "Herzlichen Glückwunsch, Luan!\nDu hast das Portal erreicht!",
     restartButton: "Neustart!",
   },
 
--- src/config/text-neo.ts
+++ src/config/text-neo.ts
@@ -5,14 +5,14 @@
 import { TextConfig } from "./text";
 
 export const TEXT_NEO: TextConfig = {
-  title: "NEO\nPlatformer",
+  title: "NEO\nJump",
   subtitle: "Alles Gute im neuen Jahr!",
-  copy: "Hast du am 23.3.2025 schon Pläne?\nIch würde gerne einen kleinen\nAusflug mit dir machen.",
+  copy: "Hast du am Abend vom \n10.2.2025 schon Pläne? \nIch würde gerne mit dir \netwas unternehmen \n(mit Übernachten bei mir).\n\nHier schon mal ein kleines Spiel \nfür die Zeitvertreibung.",
   startButton: "Start!",
 
   win: {
     title: "Geschafft!",
-    copy: "Herzlichen Glückwunsch, Neo!\nDu hast das Portal erreicht!\n\nVergiss nicht:\n23. März 2025 – unser Ausflug!",
+    copy: "Herzlichen Glückwunsch, Neo!\nDu hast das Portal erreicht!",
     restartButton: "Neustart!",
   },
 
